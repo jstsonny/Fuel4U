fd1dda885ea6205b1d867f194692c83bffd3ffef
Redunht <94131206+Redunht@users.noreply.github.com>
2023-09-21 13:59:53 -0500

    Login/Sign up changes
    Still working on having the forms sent through. My mac is low I am walking back from work to charge it at home.

--- website/auth.py
+++ website/auth.py
@@ -2,25 +2,34 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    data = request.form
+    print(data)
+    return render_template("login.html", boolean = True)
+
+
+@auth.route('/logout')
+def logout():
+    return "<p>logout</p>"
 
 @auth.route('/sign_up', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        email = request.form.get('email')
+        username = request.form.get('username')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         #when adding database, we are gonna add conditions to already existing user
-
-        if password1 != password2:
+        if len(username) < 8:
+            flash('Username must be at least 8 characters.', category='error')
+        elif len(password1) < 8:
+            flash('Password must be at least 8 characters.', category='error')
+        elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
-
         else:
             flash('Account created!', category='success')
-            return redirect(url_for('views.profile'))
+            #return redirect(url_for('views.profile'))
         
     return render_template("signup.html")
 
