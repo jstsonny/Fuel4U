75ae5b79ed6bb149feea12fe02d01d3037a5a5fa
Daniel Garza <danielrgarza12@gmail.com>
2023-09-21 21:01:26 -0500

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 views = Blueprint('views', __name__)
 
@@ -6,8 +6,29 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html")
 
-@views.route('/profile')
+@views.route('/profile', methods=['GET', 'POST'])
 def profile():
+    if request.method == 'POST':
+        full_name = request.form.get('fullName')
+        address_1 = request.form.get('address1')
+        address_2 = request.form.get('address2')
+        city = request.form.get('city')
+        state = request.form.get('state')
+        zip_code = request.form.get('zipcode')
+
+        if len(full_name) < 3:
+            flash('Invalid name', category='error')
+        elif len(address_1) < 6:
+            flash('Invalid address', category='error')
+        elif len(city) < 1:
+            flash('Invalid city', category='error')
+        elif len(zip_code) != 5:
+            if len(zip_code) !=10:
+                flash('Invalid zip', category='error') 
+        else:
+            flash('Account Updated!', category='success')
+            return redirect(url_for('views.getquote'))
+    
     return render_template("profile.html")
 
 @views.route('/get_quote')
