7aaa0679b8873e049fe853b2da2fb4bb515ccbf7
Sonny Jahanara <sonnyjst@gmail.com>
2023-10-11 20:53:39 -0500

    Fix navbar with authentification

--- website/auth.py
+++ website/auth.py
@@ -17,7 +17,7 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+                return redirect(url_for('views.profile'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
@@ -47,8 +47,17 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            flash('Account created!', category='success')
-            #return redirect(url_for('views.profile'))
-        
-    return render_template("signup.html")
+            user = User.query.filter_by(username=username).first()
+            if user:
+                flash('Username already exists.', category='error')
+            else:
+                new_user = User(username=username, password=generate_password_hash(password1, method='sha256'))
+                db.session.add(new_user)
+                db.session.commit()
+                login_user(new_user, remember=True)
+                flash('Account created!', category='success')
+                return redirect(url_for('views.home'))
+
+    return render_template("signup.html", user=current_user)
+
 
